4516c846a705e8f46ff445f972da43612c0cd3c3
Walther Aguirre <benjy_2976@hotmail.com>
2025-12-08 10:45:55 -0500

    correccion de importacion de bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap'
 import App from '@/App.vue'
 import router from '@/router'
 
